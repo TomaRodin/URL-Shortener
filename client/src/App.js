681f461a7e938da92c72dc11f2af55542dd4ff9c
Toma <70568187+TomaRodin@users.noreply.github.com>
2021-09-02 09:29:16 +0200

    int
    -Fixed POST request

--- client/src/App.js
+++ client/src/App.js
@@ -2,23 +2,23 @@ import { useRef, useState } from "react";
 import './App.css';
 
 function App() {
+        const [newl, setNewl] = useState()
         const link = useRef()
         const newLink = useRef()
   function Add(){
-    var data = {
-      link: "link",
-      newlink: "newLink"
-    };
+    var data = "link="+link.current.value+"&newLink="+newLink.current.value+""
+
+   
     const requestOptions = {
-      mode: "no-cors",
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ title: 'React POST Request Example' })
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: data
     };
     fetch('http://localhost:3001/add', requestOptions)
         .then(response => console.log(response))
 
-    
+
+    setNewl("Your New Link is:  http://localhost:3000/"+newLink.current.value)
   }
 
   return (
@@ -29,6 +29,7 @@ function App() {
       <input type="text" placeholder="New Link:" ref={newLink} ></input>
       <br></br>
       <button onClick={Add}>Make Link</button>
+      <div>{newl}</div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
+const { response } = require('express');
 var app = express();
 
 var jsonParser = bodyParser.json();
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.get('/', function(req, res){
@@ -13,19 +14,21 @@ app.get('/', function(req, res){
 })
 
 
-app.post('/add',jsonParser, function(req, res){
-    console.log(req.body.title)
+app.post('/add', function(req, res){
+    console.log(req.body.link)
+
     
     var array = JSON.parse(fs.readFileSync(__dirname+'/data.json', 'utf8'));
     array.push({
-        "link": req.body.body,
+        "link": req.body.link,
         "newLink": req.body.newLink
     })
     console.log('Added')
-
+    console.log(array)
     var jsonArray = JSON.stringify(array);
-    fs.writeFileSync('./data.json', jsonArray, { encoding: 'utf8', flag: 'w' });
+    fs.writeFileSync(__dirname+'/data.json', jsonArray, { encoding: 'utf8', flag: 'w' });
     res.json({success:true})
+
 })
 
 app.listen(3001);
